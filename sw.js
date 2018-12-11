affd62c5408a9658ccc1ca11ee8ced99f20f5cbf
Luat Dinh <dicoluat@gmail.com>
2018-12-12 01:09:38 +0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c3c026dabbec3904a64f.js"
+    "url": "webpack-runtime-3e4ddeabce9d985abe07.js"
   },
   {
     "url": "app-d3a439796df3365428f7.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "476366cbabd28215e86134f12d07a402"
+    "revision": "4276a23c9a3d06188f4cc4e2cdcbac27"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
