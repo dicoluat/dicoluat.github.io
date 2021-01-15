c99442de3ee065c63a04b992154d67a3a4bde4f5
Luat Dinh <dicoluat@gmail.com>
2021-01-16 01:53:07 +0700

    Update ten

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">dicoluat.github.io</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+
+      <h1 class="title">dicol</h1>
     </div>
   </div>
 </template>
